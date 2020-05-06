7d73413500bf65c6007a2c7901b704c081136317
Yi, Ke <yike5460@163.com>
2020-05-06 12:20:40 +0800

    sns support

--- sms/README.md
+++ sms/README.md
@@ -1,5 +1,11 @@
 # SMS function
-注意目前这个方案Pinpoint有金额的限制，会影响能发送短信的数量。点击此处[了解如何提高该限额](https://docs.aws.amazon.com/pinpoint/latest/userguide/channels-sms-awssupport-spend-threshold.html "了解如何提高该限额")
+目前方案的后端可以选择[SNS(Simple Notification Service)](https://aws.amazon.com/sns/ "SNS(Simple Notification Service)")或者[Pinpoint](https://aws.amazon.com/pinpoint/ "Pinpoint")来实现，通过部署时参数输入来选择何种服务实现。两者均能实现短信消息的发送功能，细节差异如下表：
+|   | 可用区域 | 发送状态 | 短信模版 | 语音 | 双向SMS消息 | 批量发送 |
+| ------------ | ------------ | ------------ | ------------ | ------------ | ------------ | ------------ |
+| SNS  | 全球可用，国内的SNS下的SMS待定 | 仅有HTTPStatusCode | 暂未支持  | 暂未支持 | 暂未支持 | 需要API实现 |
+| Pinpoint | 目前6个可用区域，地域最近的是孟买和悉尼 | DeliveryStatus和StatusCode | 支持  | 支持 | 支持 | 需要API实现 |
+
+注意Pinpoint有金额的限制，会影响能可发送短信的数量。点击此处[了解如何提高该限额](https://docs.aws.amazon.com/pinpoint/latest/userguide/channels-sms-awssupport-spend-threshold.html "了解如何提高该限额")
 
 ## install SAM
 在此我们会利用到SAM工具来实现关键服务的模版编排，编译测试和一键部署。[了解什么是SAM](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/what-is-sam.html "了解什么是SAM")以及如何[安装SAM](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html "安装SAM")
@@ -15,12 +21,23 @@ SAM CLI, version 0.47.0
 
 ## 部署SAM（API Gateway，Lambda）
 ```shell
-sam build && sam local invoke "EventFanout" -e events/event.json
-sam deploy -g (输入Project ID作为入参)
+# sam build && sam local invoke "EventFanout" -e events/event.json
+# sam deploy -g (其中SNSorPinpoint输入SNS或者Pinpoint，PinPointID输入您刚才创建的Project ID，注意您如果不用Pinpoint的话该选项可以忽略)
 ```
-登陆到您的API gateway界面然后部署该API Gateway（Prod stage）
+部署完毕后您会看到类似的输出，其中Value后的URL就是后续短信功能调用的URL。
+```html
+---------------------------------------------------------------------------------------------------------------------------
+Outputs                                                                                                                   
+---------------------------------------------------------------------------------------------------------------------------
+...
+Key                 SmsSendApiUrl                                                                                         
+Description         API Gateway endpoint URL for Prod stage for SMS sending function                                      
+Value               https://<xxxx>.execute-api.us-west-2.amazonaws.com/Prod/sms/                                      
+---------------------------------------------------------------------------------------------------------------------------
 
-## IAM policy
+```
+
+## IAM policy （如果您选择Pinpoint作为后端发送服务，目前需要手动设置此项）
 create Pinpoint policy and attach to deployed Lambda function manually
 ```json
 {
@@ -41,6 +58,5 @@ create Pinpoint policy and attach to deployed Lambda function manually
 ```shell
 curl -H "Content-Type:application/json" -X POST -d '{"Number": "+123456", "SMS": "Test message from AWS!"}' https://<xxxx>.execute-api.us-west-2.amazonaws.com/Prod/sms
 ```
-
-## 目前pinpoint支持的区域
-具体参见https://docs.aws.amazon.com/pinpoint/latest/userguide/channels-sms-countries.html
+您也可以通过Postman来操作，见下图所示
+![Postman](img/postman.png)
--- sms/src/app.py
+++ sms/src/app.py
@@ -2,27 +2,42 @@
 import boto3
 import time
 import json
+import logging
 from os import getenv
 from botocore.exceptions import ClientError
 sns = boto3.client('sns') 
 pinpoint = boto3.client('pinpoint', region_name="us-west-2")
+SNSorPinpoint = getenv('SNSorPinpoint')
 PinPointID = getenv('PinPointID')
 
-def pinpoint_handler(event, context):
+# @see https://docs.python.org/3/library/logging.html#logging-levels
+LOGGING_LEVEL = getenv('LOGGING_LEVEL', 'INFO')
 
-    # raise 1$ quota limitation for pinpoint sms sending function, refer to
-    # https://docs.aws.amazon.com/pinpoint/latest/userguide/channels-sms-awssupport-spend-threshold.html
+# Set up logging level, and stream stream to stdout if not running in Lambda.
+# (We don't want to change the basicConfig if we ARE running in Lambda).
+logger = logging.getLogger(__name__)
+if getenv('AWS_EXECUTION_ENV') is None:
+  logging.basicConfig(stream=sys.stdout, level=LOGGING_LEVEL)
+else:
+  logger.setLevel(LOGGING_LEVEL)
 
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+def handler(event, context):
+
+    logger.info('invoke event %s' % json.dumps(event, indent=4, cls=DecimalEncoder))
     content = event['body'].strip().replace('\"','').replace('\\','').replace('{','').replace('}','')
-    print(content)
     phoneNumberStart = content.find('+')
     phoneNumberEnd = content.find(',')
     msgStart = content.find(':', phoneNumberEnd)
-
-    # The phone number or short code to send the message from. The phone number
-    # or short code that you specify has to be associated with your Amazon Pinpoint
-    # account. For best results, specify long codes in E.164 format.
-    originationNumber = ""
 
     # The recipient's phone number.  For best results, you should specify the
     # phone number in E.164 format.
@@ -32,13 +47,35 @@ def pinpoint_handler(event, context):
     # The content of the SMS message.
     # message = ('This is a sample message sent from Amazon Pinpoint by using the AWS SDK for Python (Boto 3). %s' % time.asctime(time.localtime(time.time())))
     message = content[msgStart+1:].strip()
-    print('send sms %s to phone number %s' %(message, phoneNumber))
+    logger.info('send message \"%s\" to phone %s with %s' % (message, phoneNumber, SNSorPinpoint))
+    response = {}
+    if SNSorPinpoint == 'SNS':
+        response = sms_handler(message, phoneNumber)
+    else:
+        response = pinpoint_handler(message, phoneNumber)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "resultOutput": response,
+        }),
+    }
+
+def pinpoint_handler(message, phoneNumber):
+
+    # raise 1$ quota limitation for pinpoint sms sending function, refer to
+    # https://docs.aws.amazon.com/pinpoint/latest/userguide/channels-sms-awssupport-spend-threshold.html
+
+    # The phone number or short code to send the message from. The phone number
+    # or short code that you specify has to be associated with your Amazon Pinpoint
+    # account. For best results, specify long codes in E.164 format.
+    originationNumber = ""
 
     # The Amazon Pinpoint project/application ID to use when you send this message.
     # Make sure that the SMS channel is enabled for the project or application
     # that you choose.
     applicationId = PinPointID
-    print('applicationId is %s' % applicationId)
+
     # The type of SMS message that you want to send. If you plan to send
     # time-sensitive content, specify TRANSACTIONAL. If you plan to send
     # marketing-related content, specify PROMOTIONAL.
@@ -72,9 +109,6 @@ def pinpoint_handler(event, context):
                 }
             }
         )
-
-    except ClientError as e:
-        print(e.response['Error']['Message'])
     # {
     #     'MessageResponse': {
     #         'ApplicationId': 'string',
@@ -100,8 +134,11 @@ def pinpoint_handler(event, context):
     #         }
     #     }
     # }
+    except ClientError as e:
+        logger.error(e.response['Error']['Message'])
+
     else:
-        print("Message sent! result %s" % response['MessageResponse']['Result'][phoneNumber])
+        logger.info("Message sent by Pinpoint! result %s" % response['MessageResponse']['Result'][phoneNumber])
         return {
             # mock for api gw
             "statusCode": 200,
@@ -113,30 +150,60 @@ def pinpoint_handler(event, context):
             }),
         }
 
-def sms_handler(event, context):
+def sms_handler(message, phoneNumber):
 
-    # https://aws.amazon.com/cn/premiumsupport/knowledge-center/sns-sms-spending-limit-increase/
-    print("Received event: " + json.dumps(event, indent=2)) 
     # Replace following with your SNS ARN 
     sns_arn = 'arn:aws:sns:us-west-2:705247044519:SimpleSMS'
-    sns_event = event 
-    sns_event["default"] = json.dumps(event) 
+    # sns_event = event 
+    # sns_event["default"] = json.dumps(event) 
+
+    '''
+    response = client.publish(
+        TopicArn='string',
+        TargetArn='string',
+        PhoneNumber='string',
+        Message='string',
+        Subject='string',
+        MessageStructure='string',
+        MessageAttributes={
+            'string': {
+                'DataType': 'string',
+                'StringValue': 'string',
+                'BinaryValue': b'bytes'
+            }
+        }
+    )
+    '''
     try: 
         response = sns.publish( 
-            TargetArn=sns_arn,
-            PhoneNumber='+6588583978',
-            Message='Test from SNS',
+            #TargetArn=sns_arn,
+            PhoneNumber=phoneNumber,
+            Message=message,
             #MessageStructure='json', 
-            Subject="Test for SMS function" 
+            Subject="SMS function" 
         )
-        print("Message sent! result %s" % response)
+        logger.info("Message sent by SNS! result %s" % response)
+
+        # return {
+        #     'statusCode': 200,
+        #     'headers': {
+        #         'Content-Type': 'application/json',
+        #         'Access-Control-Allow-Origin': '*',
+        #         "Access-Control-Allow-Headers": "*",
+        #         "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        #     },
+        #     'body': json.dumps({
+        #         "message": "hello world",
+        #     })
+        # }
         return {
             "statusCode": 200,
             "body": json.dumps({
-                "message": "successful sending SMS!",
+                "HTTPStatusCode": response['ResponseMetadata']['HTTPStatusCode'],
                 # "location": ip.text.replace("\n", "")
+                "PlaceHolder": "NULL",
             }),
         }
     except Exception as e:
-        print(e)
+        logger.error(e)
         raise e
