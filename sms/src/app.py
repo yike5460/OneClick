9623a46a38fa94aa38c79fa7d05e06a1f23b764f
yike5460 <yike5460@163.com>
2020-08-27 12:31:35 +0800

    update event json with query params

--- sms/src/app.py
+++ sms/src/app.py
@@ -1,3 +1,18 @@
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+#
+# Author: kyiamzn@amazon.com
+# Revision: v1.0
 
 import boto3
 import time
@@ -153,7 +168,7 @@ def pinpoint_handler(message, phoneNumber):
 def sms_handler(message, phoneNumber):
 
     # Replace following with your SNS ARN 
-    sns_arn = 'arn:aws:sns:us-west-2:705247044519:SimpleSMS'
+    sns_arn = 'arn:aws:sns:us-west-2:<account id>:SimpleSMS'
     # sns_event = event 
     # sns_event["default"] = json.dumps(event) 
 
